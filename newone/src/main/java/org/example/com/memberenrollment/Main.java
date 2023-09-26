01cba7d60d4230857894d256c5c72b58e4cc29b0
Rajesh <102369775+rrjb6465@users.noreply.github.com>
2023-09-26 12:28:07 -0500

    Update Main.java

--- newone/src/main/java/org/example/com/memberenrollment/Main.java
+++ newone/src/main/java/org/example/com/memberenrollment/Main.java
@@ -1,9 +1,9 @@
 package org.example.com.memberenrollment;
 
-//package org.example.com.memberenrollment;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Nikhil
+
 @SpringBootApplication
 public class Main{
 
